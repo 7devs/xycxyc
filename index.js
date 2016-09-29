d7ae4bc0bf4cd6a99c3e286de613468e097a5245
xiongyicheng <369874203@qq.com>
2016-09-29 21:12:03 +0800

    up

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 var app = require('express')(),
-    bodyParser = require('body-parser');
+    bodyParser = require('body-parser'),
+    wechat = require('./lib/wechat.js'),
+    conf = require('./lib/config.js');
 
 app.use(bodyParser.urlencoded({
     extended : false
 }));
+
+wechat(conf.wechat);
+
+wechat.createMenu(require('./lib/menu.json'));
 
 app.use('/wxapi', require('./lib/routers/wxapi.js'));
 
